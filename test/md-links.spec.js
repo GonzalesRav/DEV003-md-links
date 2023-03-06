fcfc14b8efabc7b875af305a9058588702183645
JOKI6696 <J.GONZALESRAV@GMAIL.COM>
2023-03-06 10:29:22 -0500

    extracción de links, revisión y corrección de path absolute/relative, configuración de cli

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,17 @@
-const mdLinks = require('../');
-
+const mdLinks = require('../cli.js');
+const path = require('path');
 
 describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+  let pathFile;
+
+  it('Revisar que la ruta ingresada sea absoluta', () => {
+    pathFile = "C:\Users\Joki\LABORATORIA\DEV003-md-links";
+    expect (console.log(true));
   });
 
+  it('Convertir ruta relativa a ruta absoluta', () => {
+    pathFile = "..\DEV003-md-links";
+    expect (console.log(true));
+  });
 });
