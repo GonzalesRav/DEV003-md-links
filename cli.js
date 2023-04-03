92ec46548d3f9a01e94468b9480935a1d1e54f44
JOKI6696 <J.GONZALESRAV@GMAIL.COM>
2023-04-03 10:35:01 -0500

    Readme listo + diagrama

--- cli.js
+++ cli.js
@@ -9,10 +9,9 @@ const valStats = inputArray.includes('--stats') || inputArray.includes('--s')
 const help = inputArray.includes('--help')
 
 if (inputArray.length <= 2) {
-  console.log(inputArray);
   console.log('Por favor, introduzca datos con el formato válido. Para solicitar ayuda utilizar el comando "--help"')
 } else if (help) {
-  console.log('La api md-links necesita ingresar una ruta absoluta o relativa de un archivo o directorio \n para extraer las url y el texto de los links encontrados en los archivos ".md".\n Utilizar el comando "--validate" o "--v" para saber su estatus http.\n Utilizar el comando "--stats" o "--s" para contabilizar la cantidad total de links y links unicos encontrados en los archivos.\n Utilizar la mezcla de ambos comandos para contabilizar cuántos links "rotos" existen.');
+  console.log('La libreria md-links necesita ingresar una ruta absoluta o relativa de un archivo o directorio \n para extraer las url y el texto de los links encontrados en los archivos ".md".\n Utilizar el comando "--validate" o "--v" para saber su estatus http.\n Utilizar el comando "--stats" o "--s" para contabilizar la cantidad total de links y links unicos encontrados en los archivos.\n Utilizar la mezcla de ambos comandos para contabilizar cuántos links "rotos" existen.');
 } else if (validate) {
   MdLinks(path, {validate: true}).then((resValidateMd) => {
     if (valStats) {
