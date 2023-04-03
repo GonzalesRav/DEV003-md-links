8b29542fcb1930b2a32fe82a7cca04420bd4282c
JOKI6696 <J.GONZALESRAV@GMAIL.COM>
2023-04-03 15:50:39 -0500

    Intendo de recursividad

--- components/components.js
+++ components/components.js
@@ -13,13 +13,13 @@ const pathExists = (route) => {
 }
 
 // Fx sincrona que revisa si es un  archivo 
-const file = (route) => {
+const pathIsFile = (route) => {
   const r = fs.statSync(route) 
   return r.isFile() ? true :false
 }
 
 // Fx sincrona que revisa si es un  directorio 
-const dir = (route) => {
+const pathIsDirectory = (route) => {
   const r = fs.statSync(route) 
   return r.isDirectory() ? true :false
 }
@@ -142,16 +142,22 @@ const getFilesWithPath = (route) => {
 // Si route es file, devolver arreglo con el file
 // Si es directorio, devolver arreglo con su contenido utilizando readir
 let filesArray = []
-if (file(route)){
+if (pathIsFile(route)){
   filesArray.push(route)
-  return filesArray
 } else {
   const directoryContent = readir(route)
   const directoryContentAbsolute = eachFile(directoryContent, route)
   directoryContentAbsolute.forEach(element => {
-    let arrFiles = getFilesWithPath(element)
-    // Añadir elementos .md del arrFiles al filesArray
-    // Buscar concatenar arreglos 
+    if (pathIsFile(element)){
+      filesArray.push(element)
+    } else {
+      let arrFiles = getFilesWithPath(element)
+      console.log(element)
+      // Añadir elementos .md del arrFiles al filesArray
+      // Buscar concatenar arreglos 
+      filesArray = [...arrFiles]
+    }
+    
   })
   return filesArray
 }
@@ -160,8 +166,8 @@ if (file(route)){
 module.exports = {
   pathExists,
   toAbsolute,
-  file,
-  dir,
+  pathIsFile,
+  pathIsDirectory,
   readir,
   eachFile,
   checkMd,
@@ -170,5 +176,6 @@ module.exports = {
   extractLinks,
   urlStatus,
   stats,
-  broken
+  broken,
+  getFilesWithPath
 }
--- index.js
+++ index.js
@@ -1,34 +1,77 @@
 
-const { pathExists, toAbsolute, file, dir, checkMd, extractLinks, urlStatus, readir } = require('./components/components.js')
+const { pathExists, toAbsolute, pathIsDirectory, pathIsFile, checkMd, extractLinks, urlStatus, getFilesWithPath } = require('./components/components.js')
 
 const MdLinks = (path, options) => {
     return new Promise((resolve, reject) => {
+        let promisseResponse = []
         const absolute = toAbsolute(path)
         if (pathExists(absolute)) {
-            if (file(absolute)) {
-                if (checkMd(absolute)) {
-                    const links = extractLinks(absolute)
-                    links.then((res) => {
+            const filesArray = getFilesWithPath(absolute)
+            console.log(filesArray, 'filesArray')
+            filesArray.forEach((file) => {
+                console.log(file, 'file')
+                if (pathIsFile(file)) {
+                    if (checkMd(file)) {
+                        const links = extractLinks(file)
+                        links.then((res) => {
                         if (options.validate) {
-                        const validate = urlStatus(res)
-                        validate.then((response) => {resolve(response)})
-                    } else {
-                        resolve(res)
+                            const validate = urlStatus(res)
+                            validate.then((response) => {
+                                promisseResponse = [...response]
+                            })
+                        } else {
+                            // promisseResponse = [...res]
+                            console.log(res)
+                        }
+                        })
+                        
                     }
-                    })
-                    
-                } else {console.error('La ruta ingresada no pertenece a un archivo .md')}
-            }
-            else if (dir(absolute)) {
-                console.log('La ruta corresponde a un directorio, por favor ingresar un archivo markdown.')                
-            }
+                    // else {console.error('La ruta ingresada no pertenece a un archivo .md')}
+                }
+                else if (pathIsDirectory(absolute)) {             
+                    const logres = getFilesWithPath(absolute)
+                    console.log('holi',logres)
+                }
+            })
+        
         } else {
            reject(new Error('La ruta ingresada no existe')) 
         }
+
+        resolve(promisseResponse)
+        console.log(promisseResponse, 'PR')
     }
     
 )}
   
+// const MdLinks = (path, options) => {
+//     return new Promise((resolve, reject) => {
+//         const absolute = toAbsolute(path)
+//         if (pathExists(absolute)) {
+//             if (pathIsFile(absolute)) {
+//                 if (checkMd(absolute)) {
+//                     const links = extractLinks(absolute)
+//                     links.then((res) => {
+//                         if (options.validate) {
+//                         const validate = urlStatus(res)
+//                         validate.then((response) => {resolve(response)})
+//                     } else {
+//                         resolve(res)
+//                     }
+//                     })
+                    
+//                 } else {console.error('La ruta ingresada no pertenece a un archivo .md')}
+//             }
+//             else if (pathIsDirectory(absolute)) {             
+//                 const logres = getFilesWithPath(absolute)
+//                 console.log('holi',logres)
+//             }
+//         } else {
+//            reject(new Error('La ruta ingresada no existe')) 
+//         }
+//     }
+    
+// )}
 
 
 module.exports = {
